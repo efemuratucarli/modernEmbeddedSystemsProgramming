6cb47e3ec5fb21fe62cfa70bc68c564b3417f5c5
Efe Murat Uçarlı <ucarli19@itu.edu.tr>
2025-07-09 14:18:02 +0300

    Change implementation of the software delay

--- lesson9/tm4c123-keil/delay.c
+++ lesson9/tm4c123-keil/delay.c
@@ -1,7 +1,8 @@
 void delay(int iter);
 
 void delay(int iter){
-	int volatile counter = 0; //delay in any level of optimization
+    int volatile counter;
+    counter = 0;
 	while (counter < iter)
 	{
 		++counter;
